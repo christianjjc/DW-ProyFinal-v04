55744b3c474ef7130977bd68496b333d9adecb13
Christian Jiménez <christianjjc@gmail.com>
2022-11-29 23:55:02 -0500

    todo ok antes de agregar formulario de contacto

--- js/animaciones.js
+++ js/animaciones.js
@@ -25,7 +25,8 @@ const hideScrollElement = (element) => {
 
 const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-        if (elementInView(el, 1.25)) {
+        // if (elementInView(el, 1.25)) {
+        if (elementInView(el, 1)) {
             displayScrollElement(el);
         } else if (elementOutofView(el)) {
             hideScrollElement(el)
